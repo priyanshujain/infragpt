6da5bb9c059e1b49f587d1ff3a49322edf8b88c1
PJ <ipriyanshujain@gmail.com>
2025-03-02 22:47:39 +0530

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="infragpt",
-    version="0.1.0",
+    version="0.1.1",
     author="InfraGPT Team",
     author_email="your.email@example.com",
     description="A CLI tool that converts natural language to Google Cloud (gcloud) commands",
